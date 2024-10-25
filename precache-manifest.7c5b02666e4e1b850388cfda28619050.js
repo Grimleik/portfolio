e810665efe5b7dee7aebdcdc3d519b8d7c0564b3
pa2747fj-s <pa2747fj-s@student.lu.se>
2024-10-25 15:07:49 +0200

    Updates

---
+++ precache-manifest.7c5b02666e4e1b850388cfda28619050.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41c5fc86d7b0d525439ae262b7f47ca",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7261150d5febfd257a47",
+    "url": "/portfolio/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "0732d704ec3269465de0",
+    "url": "/portfolio/static/js/2.3998afa7.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/portfolio/static/js/2.3998afa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd536d7e87c784d7005b",
+    "url": "/portfolio/static/js/3.6687687a.chunk.js"
+  },
+  {
+    "revision": "7261150d5febfd257a47",
+    "url": "/portfolio/static/js/main.fccaad49.chunk.js"
+  },
+  {
+    "revision": "8abcc6727e675eb4fa5e",
+    "url": "/portfolio/static/js/runtime-main.cee5863f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/portfolio/static/media/logo.06e73328.svg"
+  }
+]);
