3b80e9181e3a0caf4a2224e815cc44bbce0a1887
pa2747fj-s <pa2747fj-s@student.lu.se>
2024-10-27 19:37:45 +0100

    Updates

---
+++ precache-manifest.92d8c6d8f137248e21ae108efb97c67a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e721c9342f07238503e64fcc16f322",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4e1775646931558c7c07",
+    "url": "/portfolio/static/css/main.595ba50d.chunk.css"
+  },
+  {
+    "revision": "afbfd696aed07eb8d3df",
+    "url": "/portfolio/static/js/2.ee355552.chunk.js"
+  },
+  {
+    "revision": "09dfcff8991931712817539b48cc9b6c",
+    "url": "/portfolio/static/js/2.ee355552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfd98f526ad8dace4602",
+    "url": "/portfolio/static/js/3.a4bd527f.chunk.js"
+  },
+  {
+    "revision": "4e1775646931558c7c07",
+    "url": "/portfolio/static/js/main.7020cdc5.chunk.js"
+  },
+  {
+    "revision": "4301eaaccb7674f4ea6d",
+    "url": "/portfolio/static/js/runtime-main.57644a31.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3d612777cec51dead8bb9ca76771911d.js"
+  "/portfolio/precache-manifest.92d8c6d8f137248e21ae108efb97c67a.js"
 );
 
 self.addEventListener('message', (event) => {
