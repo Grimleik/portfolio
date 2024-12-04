92d60bf5c9dfd576f42e0a7c84abed0390f50d68
root <root@patrik>
2024-12-04 15:37:01 +0100

    Added own icon and changed title.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,13 @@ import React from "react";
 export function Footer() {
     return (
         <footer className="footer">
-           Patrik Fjellstedt - Programmerare Felsökare. 
+            Patrik Fjellstedt - Programmerare.
+            
+            {/* <button
+                className="github-button"
+                onClick={window.open('https://github.com/Grimleik/portfolio', '_blank')}>
+                    Page Source
+            </button> */}
         </footer>
     );
 }
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HomePage() {
     return (
         <>
-            <h2>Home page!</h2>
+            <h2>Under construction...</h2>
         </>
     );
 }
